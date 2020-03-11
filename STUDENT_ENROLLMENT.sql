a1fe3cc4080162c3350cdcf7a7b91d24e2acd91e
Rohit2409 <53899180+Rohit2409@users.noreply.github.com>
2020-03-11 14:21:47 +0530

    Update STUDENT_ENROLLMENT.sql

--- STUDENT_ENROLLMENT.sql
+++ STUDENT_ENROLLMENT.sql
@@ -1,75 +1,98 @@
-create database lab4;
-use lab4;
-create table STUDENT 
-( 
-reg_no varchar (10),
-s_name varchar(15),
-major varchar(10),
-bdate date,
-primary key (reg_no));
+create table student
+(regno varchar2(10),
+ name varchar2(50),
+ major varchar2(5),
+ bdate date,primary key(regno)
+ );
+ insert into student values('1DB01IS01','Kiran','ISE','02-Jan-84');
+insert into student values('1DB01CS02','Divya','CSE','12-Oct-85');
+insert into student values('1DB01ME03','Babu','MECH','23-Sep-82');
+insert into student values('1DB01EC04','John','EC','21-Feb-84');
+insert into student values('1DB01CS05','Asha','CSE','07-Oct-79');
+Select * from student;
+
+create table course
+(courseid number(10),
+ cname varchar2(5),
+ dept varchar2(5),
+ primary key(courseid)
+ );
  
- create table COURSE
- (
- course_id int ,
- c_name varchar (20),
- dept varchar (10),
- primary key (course_id));
+ insert into course values(11,'DCS','CSE');
+insert into course values(22,'ADA','CSE');
+insert into course values(33,'CN','EC');
+insert into course values(44,'TD','MECH');
+insert into course values(55,'MUP','EC');
+
+
+CREATE TABLE TEXT
+(
+ ISBN NUMBER(10),
+ BOOK_TB VARCHAR2(20),
+ PUBLISHER VARCHAR2 (20),
+ AUTHOR VARCHAR2(20),
+ PRIMARY KEY(ISBN)
+ );
  
-  create table TEXTS
-  (
-  isbn int ,
-  book_title varchar(20),
-  publisher varchar (20),
-  author varchar (10),
-  primary key (isbn));
-   create table table_adoption
-   (
-   course_id int,
-   sem int,
-   isbn int,
-   primary key ( course_id,isbn),
-   foreign key (course_id) references COURSE (course_id),
-  foreign key (isbn) references TEXTS (isbn));
-   create table ENROLL
-   (
-   reg_no varchar(10),
-   course_id int,
-   sem int,
-   marks int,
-   primary key (reg_no , course_id),
-   foreign key (reg_no) references STUDENT (reg_no) ,
-   foreign key (course_id) references COURSE (course_id)
-   );
-   insert into STUDENT values ('1DB01IS01',' KIRAN', 'ISE', '1984-01-02');
-   insert into STUDENT values ('1DB01CS02','DIVYA', 'CSE',' 1985-10-12');
-   insert into STUDENT values ('1DBO1ME03', 'BABU', 'MECH', '1982-09-23');
-   insert into STUDENT values ('1DB01EC04', 'JOHN', 'E&C' ,'1984-02-21');
-   insert into STUDENT values ('1DB01CS05' ,'ASHA', 'CSE' ,'1979-10-07');
-   select * from STUDENT ;
-    insert into COURSE values (11, 'DCS', 'CSE');
-    insert into COURSE values (22, 'ADA', 'CSE');
-    insert into COURSE values (33, 'CN', 'E&C');
-    insert into COURSE values (44, 'TD', 'MECH');
-    insert into COURSE values (55, 'MUP', 'E&C');
-    select * from COURSE ;
-    insert into TEXTS values (7722,' VB',' PHI',' HOLZNER');
-	insert into TEXTS values (1144,'DSWITHC','SAPNA','NANDAGOPA');
-	insert into TEXTS values (4400,' CPROG',' PHI' ,'BALAGURU');
-    insert into TEXTS values (5566,' CN', 'PHI', 'TENENBAUM');
-    insert into TEXTS values (3388,' MP',' TATA ','BREY');
-    select * from TEXTS ;
-    insert into  table_adoption values (11, 3, 7722);
-	insert into  table_adoption values  (22, 4, 7722);
-    insert into  table_adoption values (11, 5, 4400);
-    insert into  table_adoption values  (11, 8 ,5566);
-	insert into  table_adoption  values (55, 4, 3388); 
-    insert into  table_adoption values (44, 4 ,5566);   
-    insert into  table_adoption values  (44, 7, 3388);   
-    select * from table_adoption;
-   insert into ENROLL values ('1DB01IS01' , 22, 5, 76);
-   insert into ENROLL values ('1DB01IS01', 11, 3, 90);
-   insert into ENROLL values ('1DB01CS02', 33, 6 ,55);
-   insert into ENROLL values ('1DBO1ME03', 22, 5, 75);
-   insert into ENROLL values ('1DB01CS05', 44 ,5, 75);
-   select * from ENROLL;
+INSERT INTO TEXT VALUES(7722,'VB','PHI','HOLZNER');
+INSERT INTO TEXT VALUES(1144,'DS WITH C','SAPNA','NANDAGOPAL');
+INSERT INTO TEXT VALUES(4400,'C PROG','PHI','BALAGURU');
+INSERT INTO TEXT VALUES(5566,'CN','PHI','TENENBAUM');
+INSERT INTO TEXT VALUES(3388,'MP','TATA','BREY');
  
+CREATE TABLE ADOPTION
+(
+  COURSEID NUMBER (10),
+  SEM NUMBER(1),
+  ISBN NUMBER (10),
+  PRIMARY KEY(COURSEID,ISBN),
+  FOREIGN KEY(COURSEID) REFERENCES COURSE (COURSEID) ON DELETE CASCADE,
+  FOREIGN KEY(ISBN) REFERENCES TEXT (ISBN) ON DELETE CASCADE
+  );
+  
+INSERT INTO ADOPTION VALUES (11,3,7722);
+INSERT INTO ADOPTION VALUES (22,4,7722);
+INSERT INTO ADOPTION VALUES (11,5,4400);
+INSERT INTO ADOPTION VALUES (11,8,5566);
+INSERT INTO ADOPTION VALUES (55,4,3388);
+INSERT INTO ADOPTION VALUES (44,4,5566);
+INSERT INTO ADOPTION VALUES (44,7,3388);
+
+
+CREATE TABLE ENROLL
+(
+ REGNO VARCHAR(10),
+ COURSEID NUMBER(10),
+ SEM NUMBER(1),
+ MARKS NUMBER(10),
+PRIMARY KEY(COURSEID,REGNO),
+FOREIGN KEY(COURSEID) REFERENCES COURSE (COURSEID) ON DELETE CASCADE,
+FOREIGN KEY(REGNO) REFERENCES STUDENT (REGNO) ON DELETE CASCADE
+  );
+INSERT INTO ENROLL VALUES('1DB01IS01',22,5,76);
+INSERT INTO ENROLL VALUES('1DB01IS01',11,3,90); 
+INSERT INTO ENROLL VALUES('1DB01CS02',33,6,55);
+INSERT INTO ENROLL VALUES('1DB01ME03',22,5,75);
+INSERT INTO ENROLL VALUES('1DB01CS05',44,5,75);
+SELECT * FROM ENROLL;
+SELECT *FROM ADOPTION;
+SELECT *FROM TEXT;
+SELECT * FROM COURSE;
+SELECT * FROM STUDENT;
+
+/*DEMONSTRATE HOW YOU ADD NEW TEXT BOOK TO THE DATABASE AND MAKE THAT BOOK IS ADOPTED BY SOME DEPARTMENT.*/
+
+INSERT INTO TEXT VALUES(1801,'OS CONCEPTS','WILEY','ABRAHAM');
+INSERT INTO ADOPTION VALUES(11,3,1801); 
+
+/*)PRODUCE A LIST OF TEXTBOOKS(INCLUDE COURSE,BOOK_ISBN,BOOK_TITLE) IN THE ALPHABETIC ORDER FOR COURSES OFFERED BY THE ‘CSE’ 
+DEPT THAT USE MORE THAN TWO BOOKS.*/
+SELECT COURSE.COURSEID,COURSE.CNAME,ADOPTION.ISBN,TEXT.BOOK_TB
+FROM COURSE, ADOPTION, TEXT
+WHERE COURSE.COURSEID=ADOPTION.COURSEID AND ADOPTION.ISBN=TEXT.ISBN and dept='cse'
+ORDER BY CNAME ASC;
+
+/*)LIST ANY DEPARTMENT THAT HAS ALL ITS ADOPTED BOOKS PUBLISHED BY A SPECIFIC PUBLISHER */
+SELECT distinct COURSE.DEPT
+FROM COURSE,TEXT,ADOPTION
+WHERE TEXT.PUBLISHER='PHI' AND TEXT.ISBN=ADOPTION.ISBN AND COURSE.COURSEID=ADOPTION.COURSEID;

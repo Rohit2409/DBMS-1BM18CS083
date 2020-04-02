294fc825f7aa43f36d3990c2deb521fb4c63bc45
Rohit2409 <53899180+Rohit2409@users.noreply.github.com>
2020-04-02 15:34:18 +0530

    Updated

--- STUDENT_ENROLLMENT.sql
+++ STUDENT_ENROLLMENT.sql
@@ -1,98 +1,74 @@
-create table student
-(regno varchar2(10),
- name varchar2(50),
- major varchar2(5),
- bdate date,primary key(regno)
- );
- insert into student values('1DB01IS01','Kiran','ISE','02-Jan-84');
-insert into student values('1DB01CS02','Divya','CSE','12-Oct-85');
-insert into student values('1DB01ME03','Babu','MECH','23-Sep-82');
-insert into student values('1DB01EC04','John','EC','21-Feb-84');
-insert into student values('1DB01CS05','Asha','CSE','07-Oct-79');
-Select * from student;
+CREATE TABLE student1(
+regno varchar2(20),
+sname VARCHAR2(10),
+major VARCHAR2(20),
+bdate date,
+ primary key(regno));
 
-create table course
-(courseid number(10),
- cname varchar2(5),
- dept varchar2(5),
- primary key(courseid)
- );
- 
- insert into course values(11,'DCS','CSE');
-insert into course values(22,'ADA','CSE');
-insert into course values(33,'CN','EC');
-insert into course values(44,'TD','MECH');
-insert into course values(55,'MUP','EC');
+desc student;
+
+CREATE TABLE course(
+courseid number(20),cname VARCHAR2(20),
+deptid varchar2(20),
+PRIMARY KEY(courseid));
+
+desc faculty;
+
+CREATE TABLE text(
+isbn number(20),
+book_title varchar2(100),
+publisher VARCHAR(20),
+author varchar2(100),
+PRIMARY KEY(isbn)
+);
+
+CREATE TABLE adoption(
+courseid number(30),
+isbn number(20),
+sem number(30),
+foreign key(courseid) references course(courseid),
+foreign key(isbn) references text(isbn)
+);
+
+create table enroll(
+regno varchar2(30),
+courseid number(20),
+sem number(10),
+marks number(20),
+foreign key(regno) references student1(regno),
+foreign key(courseid) references course(courseid));
+
+insert into student1 values('1DB01IS01','KIRAN','ISE','02-JAN-84');insert into student1 values ('1DB01CS02','DIVYA','CSE','12-OCT-85');
+insert into student1 values('1DBO1ME03','BABU','MECH','23-SEP-82');
+insert into student1 values('1DB01EC04','JOHN','EC','21-FEB-84');
+insert into student1 values('1DB01CS05','ASHA','CSE','07-OCT-7');
+insert into course values(11,'DCS','CSE');
+insert into course values('22','ADA','CSE');
+insert into course values('33','CN','EC');
+insert into course values('44','TD','MECH');
+insert into course values('55','MUP','EC');
+insert into text values('7722','VB','PHI','HOLZNER');
+insert into text values('1144','DS WITH C','SAPNA','NANDAGOPAL');
+insert into text values('4400','C PROG','PHI','BALAGURU');
+insert into text values('5566','CN','PHI','TENENBAUM');
+INSERT INTO text VALUES('3388','MP','TATA','BREY');
+
+INSERT INTO ADOPTION VALUES(11,7722,3);
+INSERT INTO ADOPTION VALUES(22,7722,4);
+INSERT INTO ADOPTION VALUES(11,4400,5);
+INSERT INTO ADOPTION VALUES(11,5566,8);
+INSERT INTO ADOPTION VALUES(55,3388,4);
+INSERT INTO ADOPTION VALUES(44,5566,4);
+INSERT INTO ADOPTION VALUES(44,3388,7);
 
 
-CREATE TABLE TEXT
-(
- ISBN NUMBER(10),
- BOOK_TB VARCHAR2(20),
- PUBLISHER VARCHAR2 (20),
- AUTHOR VARCHAR2(20),
- PRIMARY KEY(ISBN)
- );
- 
-INSERT INTO TEXT VALUES(7722,'VB','PHI','HOLZNER');
-INSERT INTO TEXT VALUES(1144,'DS WITH C','SAPNA','NANDAGOPAL');
-INSERT INTO TEXT VALUES(4400,'C PROG','PHI','BALAGURU');
-INSERT INTO TEXT VALUES(5566,'CN','PHI','TENENBAUM');
-INSERT INTO TEXT VALUES(3388,'MP','TATA','BREY');
- 
-CREATE TABLE ADOPTION
-(
-  COURSEID NUMBER (10),
-  SEM NUMBER(1),
-  ISBN NUMBER (10),
-  PRIMARY KEY(COURSEID,ISBN),
-  FOREIGN KEY(COURSEID) REFERENCES COURSE (COURSEID) ON DELETE CASCADE,
-  FOREIGN KEY(ISBN) REFERENCES TEXT (ISBN) ON DELETE CASCADE
-  );
-  
-INSERT INTO ADOPTION VALUES (11,3,7722);
-INSERT INTO ADOPTION VALUES (22,4,7722);
-INSERT INTO ADOPTION VALUES (11,5,4400);
-INSERT INTO ADOPTION VALUES (11,8,5566);
-INSERT INTO ADOPTION VALUES (55,4,3388);
-INSERT INTO ADOPTION VALUES (44,4,5566);
-INSERT INTO ADOPTION VALUES (44,7,3388);
-
-
-CREATE TABLE ENROLL
-(
- REGNO VARCHAR(10),
- COURSEID NUMBER(10),
- SEM NUMBER(1),
- MARKS NUMBER(10),
-PRIMARY KEY(COURSEID,REGNO),
-FOREIGN KEY(COURSEID) REFERENCES COURSE (COURSEID) ON DELETE CASCADE,
-FOREIGN KEY(REGNO) REFERENCES STUDENT (REGNO) ON DELETE CASCADE
-  );
 INSERT INTO ENROLL VALUES('1DB01IS01',22,5,76);
-INSERT INTO ENROLL VALUES('1DB01IS01',11,3,90); 
+INSERT INTO ENROLL VALUES('1DB01IS01',11,3,90);
 INSERT INTO ENROLL VALUES('1DB01CS02',33,6,55);
-INSERT INTO ENROLL VALUES('1DB01ME03',22,5,75);
 INSERT INTO ENROLL VALUES('1DB01CS05',44,5,75);
-SELECT * FROM ENROLL;
-SELECT *FROM ADOPTION;
-SELECT *FROM TEXT;
-SELECT * FROM COURSE;
-SELECT * FROM STUDENT;
-
-/*DEMONSTRATE HOW YOU ADD NEW TEXT BOOK TO THE DATABASE AND MAKE THAT BOOK IS ADOPTED BY SOME DEPARTMENT.*/
-
-INSERT INTO TEXT VALUES(1801,'OS CONCEPTS','WILEY','ABRAHAM');
-INSERT INTO ADOPTION VALUES(11,3,1801); 
-
-/*)PRODUCE A LIST OF TEXTBOOKS(INCLUDE COURSE,BOOK_ISBN,BOOK_TITLE) IN THE ALPHABETIC ORDER FOR COURSES OFFERED BY THE ‘CSE’ 
-DEPT THAT USE MORE THAN TWO BOOKS.*/
-SELECT COURSE.COURSEID,COURSE.CNAME,ADOPTION.ISBN,TEXT.BOOK_TB
-FROM COURSE, ADOPTION, TEXT
-WHERE COURSE.COURSEID=ADOPTION.COURSEID AND ADOPTION.ISBN=TEXT.ISBN and dept='cse'
-ORDER BY CNAME ASC;
-
-/*)LIST ANY DEPARTMENT THAT HAS ALL ITS ADOPTED BOOKS PUBLISHED BY A SPECIFIC PUBLISHER */
-SELECT distinct COURSE.DEPT
-FROM COURSE,TEXT,ADOPTION
-WHERE TEXT.PUBLISHER='PHI' AND TEXT.ISBN=ADOPTION.ISBN AND COURSE.COURSEID=ADOPTION.COURSEID;
+select * from course
+select * from text;
+select * from adoption;
+select * from enroll;
+select b.courseid,t.isbn,t.book_title from adoption b,text t,course c where c.courseid=b.courseid and c.deptid='CSE' and b.isbn=t.isbn and c.courseid in (select courseid from (select courseid from adoption group by courseid,isbn) group by courseid having count(*)>=2) order by t.book_title;
+ select distinct c.deptid from course c where not exists ((select b.isbn from adoption b,course c1 where c1.courseid=b.courseid and c1.deptid=c.deptid) minus (select isbn from text where publisher='PHI')) and exists (select b.isbn from adoption b,course c1 where c1.courseid=b.courseid and c1.deptid=c.deptid);
